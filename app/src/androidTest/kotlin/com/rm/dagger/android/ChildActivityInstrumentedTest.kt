199da34191a201f527342dd22f8040a457526255
Rustam Gaifullin <rustam.gaifullin@gmail.com>
2017-05-26 22:53:17 +0200

    add worked instrument tests.

--- app/src/androidTest/kotlin/com/rm/dagger/android/ChildActivityInstrumentedTest.kt
+++ app/src/androidTest/kotlin/com/rm/dagger/android/ChildActivityInstrumentedTest.kt
@@ -4,7 +4,7 @@ import android.support.test.filters.SmallTest
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import com.rm.dagger.android.special.SpecialMessage
-import com.rm.dagger.android.ui.MainActivity
+import com.rm.dagger.android.ui.ChildActivity
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
 import org.junit.Rule
@@ -14,16 +14,15 @@ import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 @SmallTest
-class MainActivityInstrumentedTest {
+class ChildActivityInstrumentedTest {
 
     @JvmField
     @Rule
-    val mActivityTestRule = ActivityTestRule(MainActivity::class.java)
+    val mActivityTestRule = ActivityTestRule(ChildActivity::class.java)
 
     @Test
     fun checkSpecialMessage() {
         assertNotNull(mActivityTestRule.activity.specialMessage)
-        assertEquals(mActivityTestRule.activity.specialMessage, SpecialMessage("Special for Main Activity"))
+        assertEquals(mActivityTestRule.activity.specialMessage, SpecialMessage("Special for Child Activity"))
     }
-
 }
